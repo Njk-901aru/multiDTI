7c5ab57ff0ed8922878cb13034223e910d8ad1ef
渡邊成望 <watanabenarumi@watanabeseimochinoMacBook-Air.local>
2020-12-21 20:13:45 +0900

    evaluation mode

--- evaluation.py
+++ evaluation.py
@@ -27,11 +27,11 @@ from chainer.optimizer_hooks import WeightDecay
 #-------------------------------------------------------------
  # featurevector size
 plensize= 20
-#------------------------------------------------------------- 
+#-------------------------------------------------------------
 def main():
-    
+
     START = time.time()
-    
+
     import argparse as arg
     parser = arg.ArgumentParser(description='ecfpWD_n2v')
     parser.add_argument('--gpu', '-g', type=int, default=1, help='GPU ID')
@@ -57,73 +57,74 @@ def main():
     parser.add_argument('--n_hid5', type=int, default=60)
     parser.add_argument('--n_out', type=int, default=1)
     parser.add_argument('--prosize', type=int, default=5762)
-    parser.add_argument('--input', '-i', default='./result_hard')
+    parser.add_argument('--input', '-i', default='./dataset/hard_dataset')
+    parser.add_argument('--output', '-o', default='./result/hard_dataset')
     parser.add_argument('--place', '-p', default='./ecfpN2vc_mSGD/pattern')
     parser.add_argument('--frequency', type=int, default=1)
     args = parser.parse_args(args=[])
-    
+
     for i in range(5):
         print('Making Scoring Dataset...')
-        ecfp = np.load('./dataset/hard_dataset/cv_'+str(i)+'/test_fingerprint.npy')
+        ecfp = np.load(args.input+'/cv_'+str(i)+'/test_fingerprint.npy')
         ecfp = np.asarray(ecfp, dtype='float32').reshape(-1,1024)
-        file_interactions=np.load('./dataset/hard_dataset/'+'cv_'+str(i)+'/test_interaction.npy')
+        file_interactions=np.load(args.input+'cv_'+str(i)+'/test_interaction.npy')
         print('Loading labels: test_interaction.npy')
-        file_sequences=np.load('./dataset/hard_dataset/'+'cv_'+str(i)+'/test_reprotein.npy')
+        file_sequences=np.load(args.input+'cv_'+str(i)+'/test_reprotein.npy')
         print('Loading sequences: test_protein.npy')
-        
-        cID = np.load('./dataset/hard_dataset/'+'cv_'+str(i)+'/test_chemIDs.npy')
+
+        cID = np.load(args.input+'cv_'+str(i)+'/test_chemIDs.npy')
         print('Loading chemIDs: test_chemIDs.npy')
-        with open('./dataset/hard_dataset/'+'cv_'+str(i)+'/test_proIDs.txt') as f:
+        with open(args.input+'cv_'+str(i)+'/test_proIDs.txt') as f:
             pID = [s.strip() for s in f.readlines()]
         print('Loading proIDs: test_proIDs.txt')
-        
+
         n2v_c, n2v_p = [], []
-        with open('modelpp.pickle', mode='rb') as f:
+        with open('./data_multi/modelpp.pickle', mode='rb') as f:
             modelpp = pickle.load(f)
-        with open('modelcc.pickle', mode='rb') as f:
+        with open('./data_multi/modelcc.pickle', mode='rb') as f:
             modelcc = pickle.load(f)
         for j in cID:
             n2v_c.append(modelcc.wv[str(j)])
         for k in pID:
             n2v_p.append(modelpp.wv[k])
-            
+
         interactions = np.asarray(file_interactions, dtype='int32').reshape(-1,1)
         sequences = np.asarray(file_sequences, dtype='float32').reshape(-1,1,args.prosize,plensize)
         n2vc = np.asarray(n2v_c, dtype='float32').reshape(-1,128)
         n2vp = np.asarray(n2v_p, dtype='float32').reshape(-1,128)
-        
+
         # reset memory
         del file_interactions, file_sequences, n2v_c, n2v_p, cID, pID, modelcc, modelpp
         gc.collect()
-        
+
         borders = [len(interactions) * l // 100 for l in range(100+1)]
-        
+
         def cupy(x):
             return cp.array(x)
-        
+
         print(interactions.shape, ecfp.shape, sequences.shape, n2vc.shape, n2vp.shape)
         #-------------------------------
         print('Evaluater is  running...')
-        
+
         #-------------------------------
-        
+
         # Set up a neural network to evaluate
         model = MV.CNN(args.prosize, plensize, args.batchsize, args.s1, args.sa1, args.s2, args.sa2, args.s3, args.sa3, args.j1, args.pf1, args.ja1, args.j2, args.pf2, args.ja2, args.j3, args.pf3, args.ja3, args.n_hid3, args.n_hid4, args.n_hid5, args.n_out)
         model.compute_accuracy = False
         model.to_gpu(args.gpu)
-        
-        f = open(args.input+'/'+args.place+str(i)+'/evaluation_'+str(i)+'.csv', 'w') 
-        
-        #-------------------------------    
+
+        f = open(args.output+'/'+args.place+str(i)+'/evaluation_'+str(i)+'.csv', 'w')
+
+        #-------------------------------
         print("epoch","TP","FN","FP","TN","Loss","Accuracy","B_accuracy","Sepecificity","Precision","Recall","F-measure","AUC","AUPR", sep="\t")
         f.write("epoch,TP,FN,FP,TN,Loss,Accuracy,B_accuracy,Sepecificity,Precision,Recall,F-measure,AUC,AUPR\n")
-        
+
         for epoch in range(args.frequency, args.epoch+1 ,args.frequency):
             pred_score,loss =[],[]
-            
+
             with cp.cuda.Device(args.gpu):
-                serializers.load_npz(args.input+'/'+args.place+str(i)+'/model_snapshot_' + str(epoch), model)
-                
+                serializers.load_npz(args.output+'/'+args.place+str(i)+'/model_snapshot_' + str(epoch), model)
+
             for m in range(100):
                 with cp.cuda.Device(args.gpu):
                     with chainer.using_config('train', False):
@@ -141,7 +142,7 @@ def main():
                 pred_score.extend(pred_tmp.reshape(-1).tolist())
                 #pred_Auc.extend(pred_auc.reshape(-1).tolist())
                 loss.append(loss_tmp.tolist())
-                
+
             loss = np.mean(loss)
             pred_score = np.array(pred_score).reshape(-1,1)
             pred = 1*(pred_score >=0.5)
@@ -150,29 +151,26 @@ def main():
             count_FP = np.sum(np.logical_and(shape_interactions != pred, pred == 1)*1)
             count_FN = np.sum(np.logical_and(shape_interactions != pred, pred == 0)*1)
             count_TN = np.sum(np.logical_and(shape_interactions == pred, pred == 0)*1)
-            
+
             Accuracy = (count_TP + count_TN)/(count_TP+count_FP+count_FN+count_TN)
             Sepecificity = count_TN/(count_TN + count_FP)
             Precision = count_TP/(count_TP+count_FP)
             Recall = count_TP/(count_TP+count_FN)
             Fmeasure = 2*Recall*Precision/(Recall+Precision)
             B_accuracy = (Sepecificity+Recall)/2
-            
+
             AUC = metrics.roc_auc_score(shape_interactions, pred_score, average = 'weighted')
             AUPR = metrics.average_precision_score(shape_interactions, pred_score, average = 'weighted')
-            
+
             print(epoch,count_TP,count_FN,count_FP,count_TN,loss,Accuracy,B_accuracy,Sepecificity,Precision,Recall,Fmeasure,AUC,AUPR, sep="\t")
             text = '{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13}\n'.format(
                 epoch,count_TP,count_FN,count_FP,count_TN,loss,Accuracy,B_accuracy,Sepecificity,Precision,Recall,Fmeasure,AUC,AUPR)
             f.write(text)
-            
+
         f.close()
         del borders, model, f, sequences, interactions, x_gpu, y_gpu, t_gpu, n2vc_gpu, n2vp_gpu, pred_tmp, loss_tmp, count_TP,count_FN,count_FP,count_TN,loss,Accuracy,B_accuracy,Sepecificity,Precision,Recall,Fmeasure,AUC,AUPR
         gc.collect()
 
-#------------------------------- 
+#-------------------------------
 if __name__ == '__main__':
     main()
-
-
-
